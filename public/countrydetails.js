ce5dd603b5a672310542c11e6764df680b9968d5
John Philip <developerphilo@gmail.com>
2021-10-24 04:48:22 -0700

    updated changes
    - configured /counties route
    - Added counties data

--- public/countrydetails.js
+++ public/countrydetails.js
@@ -2955,6 +2955,7 @@ const countryData={
           "name": "Prisons"
         }
       ],
+      ""
 }
 
 module.exports=countryData
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 
-const Data=require('../public/countrydetails')
-
+const Data=require('../public/countrydetails');
+const countiesData=require('../public/counties');
+// getting county details
 app.get('/', (req, res) => {
-  res.send(Data)
+  res.status(200).send(Data);
+})
+// county details
+app.get('/counties', (req, res) => {
+  res.status(200).send(countiesData);
 })
 
 app.listen(port, () => {
